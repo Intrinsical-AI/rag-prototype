f75c00cdb2b1ccbd019efb770b8ef60853607d82
Intrinsical-AI <intrinsicalai@proton.me>
2026-02-25 13:26:20 +0100

    refactor(cli-structure): move docs commands into docs package namespace

---
+++ src/local_rag_backend/cli_commands/docs/__init__.py
@@ -0,0 +1,11 @@
+"""Document-related CLI commands."""
+
+from local_rag_backend.cli_commands.docs.docs_bootstrap import bootstrap_cmd
+from local_rag_backend.cli_commands.docs.docs_ingest import ingest_cmd
+from local_rag_backend.cli_commands.docs.docs_mutate import mutate_docs_cmd
+
+__all__ = [
+    "bootstrap_cmd",
+    "ingest_cmd",
+    "mutate_docs_cmd",
+]
